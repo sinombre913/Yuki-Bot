24f402bb197b42f990be8686c34d7f83ba9e451b
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-13 12:25:05 -0600

    Update Tools•setdev.js

--- plugins/Tools•setdev.js
+++ plugins/Tools•setdev.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, isRowner }) => {
   if (!text) return m.reply('✐ Por favor, proporciona un dev para el bot.\nEjemplo: setdev Yue-Bot');
   const texto2bot = '© ⍴᥆ᥕᥱrᥱძ: '
-  global.textbot = `${texto2bot + text}`;
+  global.dev = `${texto2bot + text}`;
   
   m.reply(`✐ El nombre del bot ha sido cambiado a: ${global.dev}`);
 };
