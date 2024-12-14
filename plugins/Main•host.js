b3ed00366b379cb3dce73d2cc5fc5c30f5d952cc
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-14 15:12:35 -0600

    Update Main•host.js

--- plugins/Main•host.js
+++ plugins/Main•host.js
@@ -1,30 +1,55 @@
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 
-let txt = `Hola Buenas Acá ઈ𓅇𝐂𝐮𝐞𝐫𝐯𝐨𝐎𝐅𝐂𓆰ࣩ֟፝𓆪, Cuervo-Team-Supreme Creo Un Bot Totalmente Editable Desde WhatsApp, La Mejor Parte Es Que El Bot Está Como Pre-Bot En El Mejor Hosting De Calidad, Prueba Yue-Bot Con HostingPy Lo Mejor En Calidad https://dahs.hostingpy.shop
+let txt = `╭──═[ ~_YUE-BOT_~ ]═────⋆
+│╭───────────────···
+┴│✯ *!𝐀𝐩𝐫𝐞𝐧𝐝𝐞 𝐚 𝐞𝐝𝐢𝐭𝐚𝐫 𝐘𝐮𝐞 𝐁𝐨𝐭!* 
+✩│✯ 𝐄𝐧 𝐞𝐬𝐭𝐞 𝐭𝐮𝐭𝐨𝐫𝐢𝐚𝐥 𝐭𝐞 𝐞𝐱𝐩𝐥𝐢𝐜𝐨 
+✩│✯ *𝐩𝐚𝐬𝐨 𝐚 𝐩𝐚𝐬𝐨* 𝐜𝐨𝐦𝐨 𝐩𝐞𝐫𝐬𝐨𝐧𝐚𝐥𝐢𝐳𝐚𝐫 
+✩│✯ 𝐲 𝐨𝐩𝐭𝐢𝐦𝐢𝐳𝐚𝐫 𝐭𝐮 𝐛𝐨𝐭 𝐝𝐞 𝐟𝐨𝐫𝐦𝐚 𝐟𝐚𝐜𝐢𝐥 
+✩│✯ 𝐲 𝐫𝐚𝐩𝐢𝐝𝐚. 𝐏𝐞𝐫𝐟𝐞𝐜𝐭𝐨 𝐬𝐢 𝐞𝐬𝐭𝐚𝐬 
+✩│✯ 𝐜𝐨𝐦𝐞𝐧𝐳𝐚𝐧𝐝𝐨 𝐨 𝐪𝐮𝐢𝐞𝐫𝐞𝐬 
+┬│✯ 𝐦𝐞𝐣𝐨𝐫𝐚𝐫 𝐭𝐮𝐬 𝐡𝐚𝐛𝐢𝐥𝐢𝐝𝐚𝐝𝐞𝐬.  
+│╰────────────────··· 
+╰───────────═┅═──────────
 
-(no me vendí y en el canal próximamente se verán anuncios de HostingPy)` 
+> ╭──═[ ~_ENLACES_~ ]═────⋆ 
+> │╭───────────────···
+> ┴│✯ *𝐀𝐩𝐨𝐲𝐚𝐦𝐞 𝐜𝐨𝐧 𝐭𝐮 𝐥𝐢𝐤𝐞* 
+> ✩│✯ 𝐲 𝐜𝐨𝐦𝐞𝐧𝐭𝐚 𝐪𝐮𝐞 𝐭𝐞 𝐩𝐚𝐫𝐞𝐜𝐢𝐨. 
+> ✩│✯ \`𝚃𝚞𝚝𝚘𝚛𝚒𝚊𝚕 𝙴𝚍𝚒𝚌𝚒𝚘𝚗\`
+> ✩│✯ (https://youtu.be/L2Ax3yVoZas?si=Nn5w4IopURvw7T-Z) 
+> ✩│✯ \`𝙶𝚒𝚝𝚑𝚞𝚋\`
+> ✩│✯ (https://github.com/Nimodo83/Yue-Bot)
+> ✩│✯ \`𝙷𝙾𝚂𝚃𝙸𝙽𝙶𝙿𝚈\`
+> ✩│✯ (https://dahs.hostingpy.shop)
+> ✩│✯ \`𝚂𝙺𝚈 𝚄𝙻𝚃𝚁𝙰 𝙿𝙻𝚄𝚂\`
+> ✩│✯ (https://dash.skyultraplus.com)
+> ✩│✯ \`𝚃𝙺-𝙷𝙾𝚂𝚃\`
+> ┬│✯ (https://dash.tk-joanhost.com)
+> │╰────────────────··· 
+> ╰───────────═┅═──────────`
 
 await conn.sendMessage(m.chat, { text: txt,
 contextInfo:{
 forwardedNewsletterMessageInfo: { 
-newsletterJid: '120363319941046209@newsletter', 
+newsletterJid: '120363229122990393@newsletter', 
 serverMessageId: '', 
-newsletterName: 'HostingPy' }, 
+newsletterName: 'Hostings De Yue-Bot' }, 
 forwardingScore: 9999999,
 isForwarded: true, 
 "externalAdReply": {
 "showAdAttribution": true,
 "containsAutoReply": true,
-title: `HostingPy Un Host De Calidad`,
+title: `Hostings De Calidad`,
 body: dev,
 "previewType": "PHOTO",
-thumbnailUrl: 'https://files.catbox.moe/y72e4j.jpg', 
+thumbnailUrl: 'https://files.catbox.moe/8b91ry.jpg', 
 sourceUrl: 'https://whatsapp.com/channel/0029VaMi8cn9cDDQaoeY7P3u'}}},
-{ quoted: m })
+{ quoted: estilo })
 } 
 
 handler.help = ['hostinhpy']
 handler.tags = ['main']
-handler.command = ['hostingpy', 'host']
+handler.command = ['hostingpy', 'skyplus', 'skyultra', 'skyultraplus', 'ultraplus', 'tk-host', 'tkhost', 'hosting', 'host']
 
 export default handler
