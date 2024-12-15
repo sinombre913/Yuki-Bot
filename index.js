5a2370191fb7c232b565fcda93fab3acce18732b
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-15 15:38:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/* ARCHIVO EDITADO , CREADO O MEJORADO
+POR EL CUERVO 
+CUERVO-TEAM-SUPREME 
+SE DEJAN CREDITOS PERRAS ES DE CUERVO 
+USO NO PRIVADO ES PUBLICO
+PERO CUERVO SE ENCARGA 
+*/
 import { join, dirname } from 'path';
 import { createRequire } from 'module';
 import { fileURLToPath } from 'url';
