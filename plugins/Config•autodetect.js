8f2f6c74a418ec883b2985049e00063868d9eb62
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-15 14:29:45 -0600

    Update Config•autodetect.js

--- plugins/Config•autodetect.js
+++ plugins/Config•autodetect.js
@@ -1,3 +1,10 @@
+/* ARCHIVO EDITADO , CREADO O MEJORADO
+POR EL CUERVO 
+CUERVO-TEAM-SUPREME 
+SE DEJAN CREDITOS PERRAS ES DE CUERVO 
+USO NO PRIVADO ES PUBLICO
+PERO CUERVO SE ENCARGA 
+*/
 let WAMessageStubType = (await import('@whiskeysockets/baileys')).default
 
 export async function before(m, { conn, participants, groupMetadata }) {
