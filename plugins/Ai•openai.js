79f2362aa88f9b32e9f1049a7f47d6b803d7639c
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-15 14:09:26 -0600

    Update Ai•openai.js

--- plugins/Ai•openai.js
+++ plugins/Ai•openai.js
@@ -1,27 +1,34 @@
+/* ARCHIVO EDITADO , CREADO O MEJORADO
+POR EL CUERVO 
+CUERVO-TEAM-SUPREME 
+SE DEJAN CREDITOS PERRAS ES DE CUERVO 
+USO NO PRIVADO ES PUBLICO
+PERO CUERVO SE ENCARGA 
+*/
 import Groq from 'groq-sdk';
 const handler = async (m, { conn, text, usedPrefix, command }) => {
 const groq = new Groq({ apiKey: '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' });
-    conn.sylph = conn.sylph ? conn.sylph : {};
+    conn.sendMessage = conn.sendMessage ? conn.sendMessage : {};
     let ya = text && m.quoted ? (m.quoted.text ? text + '\n\n' + m.quoted.text : text) : text ? text : (m.quoted ? (m.quoted.text ? m.quoted.text : false) : false);
     if (!ya) throw `\`\`\`[ ✐ ] Por favor ingresa un texto. Ejemplo: ${usedPrefix + command} Hola\`\`\``
       try {
         let { key } = await conn.sendMessage(m.chat, { text: wait }, { quoted: m });
         if (!(m.sender in conn.sylph))
-        conn.sylph[m.sender] = [{
+        conn.sendMessage[m.sender] = [{
           role: 'system',
           content: `Eres ${botname}, una inteligencia artificial creada por ${creadorbot}, responde de manera clara y concisa para que los usuarios entiendan mejor tus respuestas. El nombre del usuario será: ${conn.getName(m.sender)}`,
         }];
   
-        if (conn.sylph[m.sender].length > 10) {
-          conn.sylph[m.sender] = conn.sylph[m.sender].slice(-1);
+        if (conn.sendMessage[m.sender].length > 10) {
+          conn.sendMessage[m.sender] = conn.sendMessage[m.sender].slice(-1);
         }
 
-        conn.sylph[m.sender].push({
+        conn.sendMessage[m.sender].push({
           role: 'user',
           content: ya,
         });
 
-        let msg = [ ...conn.sylph[m.sender], {
+        let msg = [ ...conn.sendMessage[m.sender], {
           role: 'user',
           content: ya,
         }];
@@ -33,7 +40,7 @@ const groq = new Groq({ apiKey: 'gsk_pvUGuoYY3unKEUcIrBglWGdyb3FYRWLcTPe7H39DyzO
 
         const json = await groq.chat.completions.create(payloads)
         let message = json.choices[0].message.content;
-        conn.sylph[m.sender].push({
+        conn.sendMessage[m.sender].push({
           role: "system",
           content: message,
         });
