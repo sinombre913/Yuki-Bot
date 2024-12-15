2c80341d6171af4929e5645889449512f2e7fd48
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-15 14:15:21 -0600

    Update Buscador•yts.js

--- plugins/Buscador•yts.js
+++ plugins/Buscador•yts.js
@@ -1,3 +1,10 @@
+/* ARCHIVO EDITADO , CREADO O MEJORADO
+POR EL CUERVO 
+CUERVO-TEAM-SUPREME 
+SE DEJAN CREDITOS PERRAS ES DE CUERVO 
+USO NO PRIVADO ES PUBLICO
+PERO CUERVO SE ENCARGA 
+*/
 import yts from 'yt-search'
 
 var handler = async (m, { text, conn, args, command, usedPrefix }) => {
