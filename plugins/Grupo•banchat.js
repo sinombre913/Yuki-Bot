0429c486ee291b0f4e43708238c72f23d3d5d455
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-15 18:25:54 -0600

    Update Grupo•banchat.js

--- plugins/Grupo•banchat.js
+++ plugins/Grupo•banchat.js
@@ -1,3 +1,10 @@
+/* ARCHIVO EDITADO , CREADO O MEJORADO
+POR EL CUERVO 
+CUERVO-TEAM-SUPREME 
+SE DEJAN CREDITOS PERRAS ES DE CUERVO 
+USO NO PRIVADO ES PUBLICO
+PERO CUERVO SE ENCARGA 
+*/
 let handler = async (m) => {
 
 global.db.data.chats[m.chat].isBanned = true
