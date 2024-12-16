fa3fb8ddf51e171931aef4eb4790c02f9794c852
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-15 18:27:07 -0600

    Update Grupo•delete.js

--- plugins/Grupo•delete.js
+++ plugins/Grupo•delete.js
@@ -1,3 +1,10 @@
+/* ARCHIVO EDITADO , CREADO O MEJORADO
+POR EL CUERVO 
+CUERVO-TEAM-SUPREME 
+SE DEJAN CREDITOS PERRAS ES DE CUERVO 
+USO NO PRIVADO ES PUBLICO
+PERO CUERVO SE ENCARGA 
+*/
 let handler = async (m, { conn, usedPrefix, command }) => {
 
 if (!m.quoted) return conn.reply(m.chat, `✐ Responde al mensaje que deseas eliminar.`, m, rcanal)
