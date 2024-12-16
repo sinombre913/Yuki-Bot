db406a3dc2355994eed5d2b6f46db1e89c6daa51
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-15 18:34:16 -0600

    Update Grupo•hidetag.js

--- plugins/Grupo•hidetag.js
+++ plugins/Grupo•hidetag.js
@@ -1,3 +1,10 @@
+/* ARCHIVO EDITADO , CREADO O MEJORADO
+POR EL CUERVO 
+CUERVO-TEAM-SUPREME 
+SE DEJAN CREDITOS PERRAS ES DE CUERVO 
+USO NO PRIVADO ES PUBLICO
+PERO CUERVO SE ENCARGA 
+*/
 let handler = async (m, { conn, text }) => {
   if (!text) return conn.reply(m.chat, '✐ Por favor, ingresa un mensaje para enviar a todos los usuarios.', m);
 
