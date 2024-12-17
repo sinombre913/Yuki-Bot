2f102d957ac4486ec333b677af052bbf1499ab51
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-16 23:43:07 -0600

    Update Tools•setdespedida.js

--- plugins/Tools•setdespedida.js
+++ plugins/Tools•setdespedida.js
@@ -1,3 +1,10 @@
+/* QUEDA PROHIBIDO EDITAR ESTOS CREDITOS
+ Powered By Cuervo-Team-Supreme 
+ya se te deja editar mucho del bot deja nuestros créditos lacra no seas puerca y respeta
+att: Cuervo-Team-Supreme
+(agrega tus creditos no borres o cambies)
+*/
+
 let handler = async (m, { conn, text, isRowner }) => {
   if (!text) return m.reply('✐ Por favor, proporciona un mensaje de despedida para el bot.\nEjemplo: !setdespedida adios user');
 
