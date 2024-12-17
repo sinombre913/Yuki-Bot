60dfadd8ed71565e80a249e10e3b37d4c70b322f
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-16 23:47:17 -0600

    Update Tools•setowner.js

--- plugins/Tools•setowner.js
+++ plugins/Tools•setowner.js
@@ -1,3 +1,10 @@
+/* QUEDA PROHIBIDO EDITAR ESTOS CREDITOS
+ Powered By Cuervo-Team-Supreme 
+ya se te deja editar mucho del bot deja nuestros créditos lacra no seas puerca y respeta
+att: Cuervo-Team-Supreme
+(agrega tus creditos no borres o cambies)
+*/
+
 let handler = async (m, { conn, text, isRowner }) => {
     let amount = text.trim();
     if (!/^\d+$/.test(amount)) {
