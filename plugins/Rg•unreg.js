9300c0246f1ca2ff7dbdb0b31ed38f6f51f7181c
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-16 23:16:54 -0600

    Update Rg•unreg.js

--- plugins/Rg•unreg.js
+++ plugins/Rg•unreg.js
@@ -1,3 +1,10 @@
+/* ARCHIVO EDITADO , CREADO O MEJORADO
+POR EL CUERVO 
+CUERVO-TEAM-SUPREME 
+SE DEJAN CREDITOS PERRAS ES DE CUERVO 
+USO NO PRIVADO ES PUBLICO
+PERO CUERVO SE ENCARGA 
+*/
 import { createHash } from 'crypto';
 
 let handler = async function (m, { conn, args }) {
