7aa2a448ade400aeca2398912e9352fe7010616f
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-16 19:45:38 -0500

    Actualizar Gacha•resetpersonajes.js

--- plugins/Gacha•resetpersonajes.js
+++ plugins/Gacha•resetpersonajes.js
@@ -1,10 +1,4 @@
-/* ARCHIVO EDITADO , CREADO O MEJORADO
-POR EL CUERVO 
-CUERVO-TEAM-SUPREME 
-SE DEJAN CREDITOS PERRAS ES DE CUERVO 
-USO NO PRIVADO ES PUBLICO
-PERO CUERVO SE ENCARGA 
-*/
+// David-Chian >> https://github.com/David-Chian
 
 import fs from 'fs';
 const obtenerDatos = () => {
