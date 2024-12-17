23375168d05f8d49e254931256736d379be8c327
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-16 19:34:59 -0600

    Update Popietario•autoadmin.js

--- plugins/Popietario•autoadmin.js
+++ plugins/Popietario•autoadmin.js
@@ -1,3 +1,10 @@
+/* ARCHIVO EDITADO , CREADO O MEJORADO
+POR EL CUERVO 
+CUERVO-TEAM-SUPREME 
+SE DEJAN CREDITOS PERRAS ES DE CUERVO 
+USO NO PRIVADO ES PUBLICO
+PERO CUERVO SE ENCARGA 
+*/
 const handler = async (m, {conn, isAdmin, groupMetadata }) => {
   if (isAdmin) return m.reply('✧ *Tu ya eres adm.*');
   try {
