af15e32dbbd061a3a0a38561ecad246ec032e808
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-16 23:20:41 -0600

    Update Rpg•cartera.js

--- plugins/Rpg•cartera.js
+++ plugins/Rpg•cartera.js
@@ -1,3 +1,10 @@
+/* ARCHIVO EDITADO , CREADO O MEJORADO
+POR EL CUERVO 
+CUERVO-TEAM-SUPREME 
+SE DEJAN CREDITOS PERRAS ES DE CUERVO 
+USO NO PRIVADO ES PUBLICO
+PERO CUERVO SE ENCARGA 
+*/
 let handler = async (m, {conn, usedPrefix}) => {
 let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
 if (who == conn.user.jid) return error 
