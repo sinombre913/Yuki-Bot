089aee7cb0c583a7ca4cc8aef395779bc1e510dd
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-17 01:33:37 -0600

    Update Main•menu.js

--- plugins/Main•menu.js
+++ plugins/Main•menu.js
@@ -434,6 +434,14 @@ handler.command = ['menu', 'allmenu', 'menú', 'help'];
 handler.register = true;
 export default handler;
 
+function clockString(ms) {
+  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [d, 'd ', h, 'h ', m, 'm ', s, 's '].map(v => v.toString().padStart(2, 0)).join('')
+ }
+ 
 function pickRandom(list) {
 return list[Math.floor(Math.random() * list.length)]
 }
