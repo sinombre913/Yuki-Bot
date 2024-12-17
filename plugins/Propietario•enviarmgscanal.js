9d534881c57d8279f3749a4040728a775697a362
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-16 22:48:17 -0600

    Update and rename Propietario •enviarmgscanal.js to Propietario•enviarmgscanal.js

--- plugins/Propietario•enviarmgscanal.js
+++ plugins/Propietario•enviarmgscanal.js
@@ -1,3 +1,10 @@
+/* ARCHIVO EDITADO , CREADO O MEJORADO
+POR EL CUERVO 
+CUERVO-TEAM-SUPREME 
+SE DEJAN CREDITOS PERRAS ES DE CUERVO 
+USO NO PRIVADO ES PUBLICO
+PERO CUERVO SE ENCARGA 
+*/
 const handler = async (m, {conn, text}) => {
 
 if (!text) return await conn.reply(m.chat, `✐ Y el mensaje que quieres compartir al canal?`, m)
@@ -6,7 +13,7 @@ try {
 await m.react(rwait)
 
 await conn.reply(m.chat, `✐ El texto se envió correctamente al canal.`, m)
-await conn.reply(global.channelid, text, m)
+await conn.reply(global.channelid, text, estilo)
 await m.react(done)
 
 } catch {
