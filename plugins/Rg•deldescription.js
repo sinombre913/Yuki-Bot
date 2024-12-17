11979de0fe71f20dc0116be39f8a4550a298cd49
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-16 23:03:01 -0600

    Update Rg•deldescription.js

--- plugins/Rg•deldescription.js
+++ plugins/Rg•deldescription.js
@@ -1,5 +1,11 @@
 // OfcKing >> https://github.com/OfcKing
-
+/* ARCHIVO EDITADO , CREADO O MEJORADO
+POR EL CUERVO 
+CUERVO-TEAM-SUPREME 
+SE DEJAN CREDITOS PERRAS ES DE CUERVO 
+USO NO PRIVADO ES PUBLICO
+PERO CUERVO SE ENCARGA 
+*/
 import { createHash } from 'crypto';  
 import fetch from 'node-fetch';
 
