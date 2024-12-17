5c077f738522ba5382c0a75f9138d645258b4c1a
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-12-16 22:42:13 -0600

    Update Tools•sticker.js

--- plugins/Tools•sticker.js
+++ plugins/Tools•sticker.js
@@ -1,3 +1,10 @@
+/* ARCHIVO EDITADO , CREADO O MEJORADO
+POR EL CUERVO 
+CUERVO-TEAM-SUPREME 
+SE DEJAN CREDITOS PERRAS ES DE CUERVO 
+USO NO PRIVADO ES PUBLICO
+PERO CUERVO SE ENCARGA 
+*/
 import { sticker } from '../lib/sticker.js'
 import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
