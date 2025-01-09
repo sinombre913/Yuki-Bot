08437af113ccabc22584caeb2aa68ce2e0554392
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2025-01-09 09:18:02 -0600

    Update Tools•setwelcome.js

--- plugins/Tools•setwelcome.js
+++ plugins/Tools•setwelcome.js
@@ -8,7 +8,7 @@ att: Cuervo-Team-Supreme
 let handler = async (m, { conn, text, isRowner }) => {
   if (!text) return m.reply('✐ Por favor, proporciona una bienvenida para el bot.\nEjemplo: !setwelcome Hola user');
 
-  global.welcom1 = text.trim();
+  global.db.data.chats[m.chat].welcom1 = text.trim();
   
   m.reply(`✐ La bienvenida del bot ha sido cambiado a: ${global.welcom1}`);
 };
