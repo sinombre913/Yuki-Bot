ddb77a220d6e2dceda6d8939423dae6f1db434de
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2025-01-09 18:12:49 -0600

    Update Descargas•ytmp4.js

--- plugins/Descargas•ytmp4.js
+++ plugins/Descargas•ytmp4.js
@@ -9,7 +9,7 @@ async function dansyaytdl(link) {
             throw new Error('No se pudo obtener el token.');
         }
 
-        const url = `https://y2ts.us.kg/youtube?url=${encodeURIComponent(link)}`;
+        const url = `https://delirius-apiofc.vercel.app/download/ytmp4?url=${encodeURIComponent(link)}`;
         const headers = {
             'Authorization-Token': token,
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
