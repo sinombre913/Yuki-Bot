b48056ff6e35d7571d45c4b99f6763823bce5975
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2025-01-10 21:06:09 -0600

    Add files via upload

--- plugins/Descargas•ytmp3.js
+++ plugins/Descargas•ytmp3.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 async function dansyaytdl(link) {
     try {
-        const response = await axios.get('https://y2ts.us.kg/token');
+        const response = await axios.get('https://api.host-bot.store/api/dl/yt2/token');
         const token = response.data?.token;
 
         if (!token) {
             throw new Error('No se pudo obtener el token.');
         }
 
-        const url = `https://delirius-apiofc.vercel.app/download/ytmp3?url=${encodeURIComponent(link)}`;
+        const url = `https://api.host-bot.store/api/dl/yt2?url=${encodeURIComponent(link)}&apikey=142d875c`;
         const headers = {
             'Authorization-Token': token,
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
